7971a53bd6045b3716d3145c5543fbcc651ef373
searchme86 <searchme86@gmail.com>
2022-03-21 18:02:00 +0900

    🧑‍💻  changed import path to absolute one

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,8 +2,8 @@ import client from '@libs/server/client';
 import withHandler from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// import client from '../../../libs/server/client';
-// import withHandler from '../../../libs/server/withHandler';
+// import client from '../@libs/server/client';
+// import withHandler from '../@libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
